8dd10c4611c1b47c729c41f82dfc58fb25241a55
Houlaymatou <houlay.bah@gmail.com>
2016-08-23 16:23:43 +0200

    update shop.dev

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -7,11 +7,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Client;
+/**
+     * @Route("/users", name="clients")
+     */
 
 class ClientController extends Controller
 {
     /**
-     * @Route("/clients")
+     * @Route("/clients", name="clients")
      */
 
     public function indexAction(Request $request)
@@ -46,7 +50,32 @@ class ClientController extends Controller
         //var_dump($request->request->all());
         $params = $request->request->all();//On recupère tous les paramètres d'une requete POST; cf api.symfony.com
         //var_dump($test['name']);
-        return new Response($params['name']);
+        //**Enregister de nouveau client dans la bdd **
+
+        $client = new Client();
+        $client->setName($params['name']);
+        $client->setAge($params['age']);
+        //var_dump($client);
+        $em = $this->getDoctrine()->getManager();//Entity manager ($em par convention)
+        $em->persist($client); //persist commande d'insertion d'une requête préparer
+        $em->flush();
+        return $this->redirectToRoute('clients');
+   }
+  /**
+     * @Route("/clients/{id}/delete", name="client_delete")
+     */ 
+   public function deleteAction($id)//la variable paramètre est strictement identique à la variable de la route
+   //(id pour cet exemple)
+   {    //On récupère l'objet client grace à l'id
+        $em = $this->getDoctrine()->getManager();
+        $client = $em->getRepository('AppBundle:Client')->find($id);
+        //var_dump($client);
+        //Suppression d'un objet
+        $em->remove($client);
+        $em->flush();
+        //return new Response($id);
+        //Redirection vers client
+        return $this->redirectToRoute('clients');
    }
     
 }
