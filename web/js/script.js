b8386bbdda16df6d81f1b010c298f62a05ffe513
Houlaymatou <houlay.bah@gmail.com>
2016-08-30 10:48:06 +0200

    ajax

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,23 @@
 $('.delete').click(function(){
-var id = $(this).attr('id');
-console.log(id);
+	var elem = $(this);//copie de la balise a
+   var id = elem.attr('id');//récupèration de l'id
+
+//console.log(id);
+//requête ajax
+$.ajax({
+	type:"POST",
+	url:'/admin/categories/' + id + '/delete',
+	success: function(){
+		//succès retire lélement tr du Dom 
+		elem.parent().parent().remove();
+		//affiche un message de succès
+		$('#message')
+		.html("<em>Suppression réussie</em>")
+		.animate({opacity: 0.3}, 5000,
+		function(){
+			$('#message').html("");
+		});//fais disparaitre le message au bout de 5s
+	}
+});
+
 });
